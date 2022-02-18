a4cdacba95b3887e4c3531b34b245eeab15823cf
長友啓祐 <zhangyouqiyou@gmail.com>
2022-02-18 15:16:32 +0900

    新規登録時のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+
   validates :nickname,           presence: true
-  validates :last_name,          presence: true
-  validates :first_name,         presence: true
-  validates :last_name_kana,     presence: true
-  validates :first_name_kana,    presence: true
+  validates :last_name,          presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: '全角（漢字・ひらがな・カタカナ）での入力が必須' }
+  validates :first_name,         presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: '全角（漢字・ひらがな・カタカナ）での入力が必須' }
+  validates :last_name_kana,     presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: '全角（カタカナ）での入力が必須' }
+  validates :first_name_kana,    presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: '全角（カタカナ）での入力が必須' }
   validates :date_of_birth,      presence: true
+
+  
 
 end
