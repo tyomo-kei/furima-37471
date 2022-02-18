1d414d0d2f3a29593e3ec2b73f6f3565701080f6
長友啓祐 <zhangyouqiyou@gmail.com>
2022-02-18 18:18:01 +0900

    HTMLのコメント削除、rubocopを実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,14 @@ class ApplicationController < ActionController::Base
   private
 
   def basic_auth
-    authenticate_or_request_with_http_basic do |username,password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 
   def config_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana,
+                                             :date_of_birth])
   end
 end
