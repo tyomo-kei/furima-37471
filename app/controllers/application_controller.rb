257ce715297e480ffc3934b2093639610327f38e
長友啓祐 <zhangyouqiyou@gmail.com>
2022-02-18 14:34:56 +0900

    ログイン、ログアウト昨日の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :config_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -10,4 +10,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def config_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth])
+  end
 end
