6d905cbeef20f76a5f98eb7ec2a461e493b0f14c
長友啓祐 <zhangyouqiyou@gmail.com>
2022-02-18 21:23:55 +0900

    itemモデルとテーブルを作成、userとitemのアソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items 
+  has_many :purchases
+
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }
 
