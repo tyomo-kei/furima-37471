1873d5fc55bd0ebc2ecfa639d15035d412233d41
長友啓祐 <zhangyouqiyou@gmail.com>
2022-03-13 21:01:46 +0900

    エラーログの記述追加

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -53,3 +53,6 @@ end
 after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
+
+#エラーのログを記録するファイルを指定
+stderr_path "#{app_path}/log/unicorn.stderr.log"
